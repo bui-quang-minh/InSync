8219152f214407661f5b39e90c7df1820759139e
Minh <buiquangminh731@gmail.com>
2024-08-08 20:11:51 +0700

    Rebuilded for merging

--- opencv/build/intermediates/cxx/Debug/505w2w6y/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Users\\PhanQuangHuy59\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\ProjectThesis\\ProjectInSync\\InSync\\opencv\\.cxx\\Debug\\505w2w6y\\x86" ^
-  opencv_jni_shared
