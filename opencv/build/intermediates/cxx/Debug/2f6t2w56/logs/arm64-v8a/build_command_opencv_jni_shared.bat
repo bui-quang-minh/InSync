80da1ff17925014edf243517a9a85a3150574f3e
HuyKhongBietCode <phanhuy05092002@gmail.com>
2024-08-13 14:57:52 +0700

    commit code 2 screeen log Clean project

--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Users\\PhanQuangHuy59\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\ProjectThesis\\ProjectInSync\\InSync\\opencv\\.cxx\\Debug\\2f6t2w56\\arm64-v8a" ^
-  opencv_jni_shared
