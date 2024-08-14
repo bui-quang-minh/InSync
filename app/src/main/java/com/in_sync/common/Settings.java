8ac4dae006f94a9e0045d9943c17fad22f89081a
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-08-14 10:29:17 +0700

    update

--- app/src/main/java/com/in_sync/common/Settings.java
+++ app/src/main/java/com/in_sync/common/Settings.java
@@ -2,7 +2,7 @@ package com.in_sync.common;
 
 public class Settings {
     public static final String URL = "https://api.clerk.com/v1";
-    public static final String BEARER_TOKEN = "sk_test_gm6scgZNL70mrN56QNDPA2p337v9v9qzx63TWwgTxZ";
+    public static final String BEARER_TOKEN = "sk_test_mdeOGOWMkUgVnPB86kBftM9ASdjzXFQRNO8GnhePfR";
     public static final String GET_USER= "/users?email_address=";
     public static final String GET_ALL_USERS = "/users";
     public static final String VERIFY_PASSWORD_HEAD = "/users/";
