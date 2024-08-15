b163ad8c56b65ce29dfd4542421d81ff3f2aa34a
Dương Thành Luân <luanduong14@gmail.com>
2024-08-15 11:47:26 +0700

    Update View Image

--- app/src/main/java/com/in_sync/fragments/ScreenCaptureFragment.java
+++ app/src/main/java/com/in_sync/fragments/ScreenCaptureFragment.java
@@ -1,8 +1,9 @@
 package com.in_sync.fragments;
 
-import static android.content.Context.MEDIA_PROJECTION_SERVICE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 
@@ -10,46 +11,98 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.in_sync.R;
-import com.in_sync.activities.ScreenshotPermissionActivity;
+import com.in_sync.adapter.ImageGalleryAdapter;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.zip.Inflater;
+
 
 public class ScreenCaptureFragment extends Fragment {
     private Button captureButton;
     private Context context;
+    private String TAG = "SCREENCAPTURE";
     private static final int REQUEST_CODE_SCREEN_CAPTURE = 1;
     private MediaProjectionManager mediaProjectionManager;
+    private String FOLDER_PATH;
 
     public ScreenCaptureFragment(Context context) {
         this.context = context;
     }
 
+    // on below line we are creating variables for
+    // our array list, recycler view and adapter class.
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private ArrayList<String> imagePaths;
+    private RecyclerView imagesRV;
+    private ImageGalleryAdapter imageRVAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_screen_capture, container, false);
-        initView(view);
-        handleEvent();
+        initView();
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_screen_capture, null);
         return view;
     }
 
-    private void initView(View view) {
-        captureButton = view.findViewById(R.id.capture_button);
-        mediaProjectionManager = (MediaProjectionManager) context.getSystemService(MEDIA_PROJECTION_SERVICE);
+    private void initView() {
+        FOLDER_PATH = Objects.requireNonNull(context.getExternalFilesDir(null)).getAbsolutePath() + "/screenshots/";
     }
 
-    private void handleEvent() {
-        captureButton.setOnClickListener(this::initiateOverlay);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imagePaths = new ArrayList<>();
+        imagesRV = view.findViewById(R.id.image_gallery);
+        prepareRecyclerView();
     }
 
-    private void initiateOverlay(View view) {
-        Intent intent = new Intent(context, ScreenshotPermissionActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+    private void prepareRecyclerView() {
+        imageRVAdapter = new ImageGalleryAdapter(context, getFileName());
+        GridLayoutManager manager = new GridLayoutManager(context, 2);
+        imagesRV.setLayoutManager(manager);
+        imagesRV.setAdapter(imageRVAdapter);
     }
+
+    private List<String> getFileName() {
+        List<String> filesName = new ArrayList<>();
+        try {
+            File folder = new File(FOLDER_PATH);
+            if(folder.exists() && folder.isDirectory()) {
+                File[] files = folder.listFiles((file) -> file.getName().endsWith(".png"));
+                if(files != null) {
+                    for (File file: files) {
+                        filesName.add(FOLDER_PATH + file.getName());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(context, "ERROR: CAN NOT READ IMAGE FOLDER", Toast.LENGTH_SHORT).show();
+        }
+
+        return filesName;
+    }
+
+
 }
