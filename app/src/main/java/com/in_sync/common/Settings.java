fb01905d5c469a204ed843003eeed8abf7a89667
Minh <buiquangminh731@gmail.com>
2024-10-21 15:40:20 +0700

    Comit

--- app/src/main/java/com/in_sync/common/Settings.java
+++ app/src/main/java/com/in_sync/common/Settings.java
@@ -1,7 +1,7 @@
 package com.in_sync.common;
 
 public class Settings {
-    public static final String BASE_SYSTEM_API_URL = "https://dockerhub-insyncapi.onrender.com";
+    public static final String BASE_SYSTEM_API_URL = "https://in-sync-71cacf992634.herokuapp.com";
     public static final String URL = "https://api.clerk.com/v1";
     public static final String BEARER_TOKEN = "sk_test_mdeOGOWMkUgVnPB86kBftM9ASdjzXFQRNO8GnhePfR";
     public static final String GET_USER= "/users?email_address=";
