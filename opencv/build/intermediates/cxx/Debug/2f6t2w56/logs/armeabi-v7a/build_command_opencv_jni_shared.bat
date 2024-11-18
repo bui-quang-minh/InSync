2d286564dab88628b590054b04d79cbd8b343374
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-11-18 16:39:13 +0700

    cleaned

--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\buiqu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\InSync\\opencv\\.cxx\\Debug\\2f6t2w56\\armeabi-v7a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\buiqu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\InSync\\opencv\\.cxx\\Debug\\2f6t2w56\\x86_64" ^
-  opencv_jni_shared
